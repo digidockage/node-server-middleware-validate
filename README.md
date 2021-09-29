0b7f352d7fbf17278f571101ea86204be2fa19ee
Sgobbi Federico <federico@sgobbi.it>
2021-09-29 21:55:24 +0200

    First release

---
+++ README.md
@@ -0,0 +1,7 @@
+# Nodejs server middleware validate
+Nodejs common server middleware validate.
+
+### Install
+```
+npm install @esultocom/node-server-middleware-validate
+```
---
+++ src/index.js
@@ -0,0 +1,34 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                                 *
+ *    Copyright (c) 2021 Sgobbi Federico                                                           *
+ *    All rights reserved.                                                                         *
+ *                                                                                                 *
+ *    This file is licensed under the MIT License.                                                 *
+ *    License text available at https://opensource.org/licenses/MIT                                *
+ *                                                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// * * * * * * * * * * * * * Import externals
+const pick = require('@esultocom/node-server-util-pick');
+const ApiError = require('@esultocom/node-server-util-apierror');
+const httpStatus = require('http-status');
+const Joi = require('joi');
+
+// * * * * * * * * * * * * * The code
+const validate = (schema) => (req, res, next) => {
+  const validSchema = pick(schema, ['params', 'query', 'body']);
+  const object = pick(req, Object.keys(validSchema));
+  const { value, error } = Joi.compile(validSchema)
+    .prefs({ errors: { label: 'key' }, abortEarly: false })
+    .validate(object);
+
+  if (error) {
+    const errorMessage = error.details.map((details) => details.message).join(', ');
+    return next(new ApiError(httpStatus.BAD_REQUEST, errorMessage));
+  }
+  Object.assign(req, value);
+  return next();
+};
+
+// * * * * * * * * * * * * * Module exports
+module.exports = validate;
